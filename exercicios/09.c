6d3aae18b275540c4ea3d4423e8ba6243fc08067
rodriguestw <isaquerodrigues467@gmail.com>
2026-09-24 16:47:01 -0300

    questão 09

--- exercicios/09.c
+++ exercicios/09.c
@@ -6,5 +6,13 @@
 int main(){
     int distancia, litros, km;
 
-        
+        printf("Dintancia percorrida em quilometros: \n");
+        scanf("%d", &distancia);
+
+        printf("Quantidade de combustivel utilizado em litros: \n");
+        scanf("%d", &litros);
+
+        km = distancia / litros;
+
+        printf("Comcumo medio do veiculo: %dKm/L\n", km);
 }
