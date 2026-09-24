0a4932ec66edbf631de42b78914542a52bf64866
rodriguestw <isaquerodrigues467@gmail.com>
2026-09-24 10:02:12 -0300

    atualização

--- exercicios/02.c
+++ exercicios/02.c
@@ -6,16 +6,16 @@
 int main(){
     int a, b, c;
 
-    printf("\nDigite dois numeros inteitos:\n");
-    printf("\no primeiro numero: \n");
-    scanf("%d", &a);
+        printf("\nDigite dois numeros inteitos:\n");
+        printf("\no primeiro numero: \n");
+        scanf("%d", &a);
 
-    printf("\no segundo numero: \n");
-    scanf("%d", &b);
+        printf("\no segundo numero: \n");
+        scanf("%d", &b);
 
-    c = a + b;
+        c = a + b;
 
-    printf("\nresultado: %d\n", c);
+        printf("\nresultado: %d\n", c);
 
     return 0;
 }
