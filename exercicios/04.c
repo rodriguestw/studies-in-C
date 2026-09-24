a8f41828251c380a5036818c06e2aab61af99ccd
rodriguestw <isaquerodrigues467@gmail.com>
2026-09-24 10:02:21 -0300

    atualização

--- exercicios/04.c
+++ exercicios/04.c
@@ -1,5 +1,5 @@
-// EXERCÍCIO 4 ? Média do aluno
-// Média = (N1 + N2 + N3) / 3
+// EXERCï¿½CIO 4 ? Mï¿½dia do aluno
+// Mï¿½dia = (N1 + N2 + N3) / 3
 
 #include <stdio.h>
 #include <locale.h>
@@ -9,14 +9,14 @@ int main(){
 
     int n1, n2, n3, media;
 
-    scanf("%d\n", &n1);
-    scanf("%d\n", &n2);
-    scanf("%d\n", &n3);
+        scanf("%d\n", &n1);
+        scanf("%d\n", &n2);
+        scanf("%d\n", &n3);
 
-    media = n1 + n2 + n3 / 3;
+        media = n1 + n2 + n3 / 3;
 
-    printf("Media = %d", media);
+        printf("Media = %d", media);
 
 }
 
-// Tá errado kkkk
+// Tï¿½ errado kkkk
